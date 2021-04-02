c85445108763476711900ed63fa64aad47f0be12
Luke Gude <lukegudeios@gmail.com>
2021-04-02 09:53:50 -0400

    Project 4 Loaded

---
+++ Projects/Project 4/proj4.cpp
@@ -0,0 +1,15 @@
+//Title: proj4.cpp
+//Author: Jeremy Dixon
+//Date: 3/21/2021
+//Description: Builds a farm and starts the simulation (nothing random)
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "Farm.h"
+
+int main () {
+  Farm farm;
+  farm.StartSimulation();
+  return 0;
+}
